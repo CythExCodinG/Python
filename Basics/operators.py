dcaab872ed172f9a162aa7e6a147d0ff5246a4da
CythExCodinG <rohitiball256@gmail.com>
2024-09-23 21:37:59 +0530

    basics

---
+++ Basics/operators.py
@@ -0,0 +1,52 @@
+print(5>2)
+print(5|10)
+print(5^10)
+print(~(-10))
+print(3<<2)
+#left shift 
+print(10<<3)
+#right shift 
+print(10>>2)
+x1=5
+x2=5
+x3=5
+print(x1)
+
+#Special membersip operators
+print(x1 is x2)
+print(x1==x2)
+print(x1 is not x3)
+print(id(x1))
+print(id(x2))
+
+
+msg="This is python operators basic programs"
+print("is" in msg)
+
+l=[3,5,8,6,8]
+print(3 in l)
+
+print("Rushabh is \\n bot in \t bgmi")
+#Strings can be accessed using indexing
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Basics/strings.py
@@ -0,0 +1,21 @@
+s1="Mca is very good"
+print(s1[3])
+print(s1[3:2])
+
+print(s1[1:])
+#Negative indexing Start starts for last starts with (-1)
+print(s1[-1])
+
+#Dealing with aphostrophies
+print("Society's")
+
+#Raw Strings
+print(r"My friend \n naveen is the best")
+
+#Concatenation
+s1="Hello guys"
+s2="I am Rohit"
+s1=s1.replace("Hello","Who")
+print(s1)
+
+print(len(s1))
