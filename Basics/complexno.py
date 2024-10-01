1eaab330c2574899d8d22c485be606a8abe56971
CythExCodinG <rohitiball256@gmail.com>
2024-10-01 08:15:45 +0530

    strings and tuples

---
+++ Basics/complexno.py
@@ -0,0 +1,4 @@
+import cmath
+c1=complex(2,4)
+c2=1+5j
+print(c1+c2)
---
+++ Strings/strings2.py
@@ -0,0 +1,16 @@
+s1="hello"
+
+s2="World"
+print(id(s1[2]))
+print(id(s2[3]))
+
+# print(s1*100)
+
+s4="Rohit"
+print(s4[::-1])
+
+#various functions 
+str1="hello my name is rohit"
+x=str1.capitalize()
+print(x)
+c1=2+5
