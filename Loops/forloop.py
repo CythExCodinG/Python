c2ede6aa97fcc4646cbfab78540a7ef5fc6ef566
CythExCodinG <rohitiball256@gmail.com>
2024-10-03 23:26:08 +0530

    loops

---
+++ Loops/forloop.py
@@ -0,0 +1,10 @@
+s1=["rushabh","atharva","sahil","sahil","bablu","ayush","nachiket","sumit","Amaan","Sahil","Vivek","Vinit"]
+for i in s1:
+  print(i)
+
+value=2,3,5,7
+print(value)
+for i in range(4):
+  print(i)
+else:
+  print("TY")
---
+++ Loops/while.py
@@ -0,0 +1,10 @@
+
+no=3456
+rev=0
+while(no!=0):
+  digit=no%10
+  rev=rev*10+digit
+  no=no//10
+print(rev)
+
+int 
